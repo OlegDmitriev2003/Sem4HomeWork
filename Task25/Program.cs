743d29fae3a0da4460f5b13d6d0adde6b946cc1d
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-31 21:23:34 +0300

    finished Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int Num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int Power = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Math.Pow(Num,Power));
