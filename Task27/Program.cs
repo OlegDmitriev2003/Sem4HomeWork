fda9a23dcf1f52190ccdb74e7e036c7e8bdaab38
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-31 21:38:31 +0300

    Completed Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int sum = 0;
+Console.Write("Введите число: ");
+int Num = int.Parse(Console.ReadLine()!);
+while (Num > 0)
+{
+    sum = sum + Num%10;
+    Num = Num/10;
+}
+Console.Write($"Cумма цифр - {sum} ");
