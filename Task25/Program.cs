0fe9c290b645cd36c3e32afd7be4166f7867f253
OlegDmitriev <odimitrovgrad@gmail.com>
2023-05-31 21:50:31 +0300

    Changed solution for Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,6 +1,12 @@
 ﻿Console.Clear();
+int N = 1;
 Console.Write("Введите число A: ");
 int Num = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число B: ");
 int Power = int.Parse(Console.ReadLine()!);
-Console.WriteLine(Math.Pow(Num,Power));
+while (N < Power)
+    {
+        Num = Num*Num;
+        N++;
+    }
+    Console.Write(Num);
