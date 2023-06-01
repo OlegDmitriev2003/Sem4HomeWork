8dc4c3df92792e452f1ef76beafabe9f7c72b32c
OlegDmitriev <odimitrovgrad@gmail.com>
2023-06-01 10:39:25 +0300

    completed Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int i = 0;
+int[] array = new int [8];
+while (i < array.Length)
+{
+    Console.Write("Введите число: ");
+    array[i] = int.Parse(Console.ReadLine()!);
+    i++;
+}
+    Console.WriteLine($"Результат - [{string.Join(", ", array)}]");
